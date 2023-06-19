21197b43fe3a5251a56013c9e000d970f66ea2d5
Arshad9068 <malikarshad62045@gmail.com>
2023-06-19 14:26:34 +0530

    first commit

---
+++ src/modules/Landingpage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Mylanding()
+{
+    return "Hallo"
+}
+
+export default Mylanding
+
+export function Myfunction1()
+{
+    return <div><h1>this is mylandin1</h1><h3>hallo this is my h3</h3></div>
+}
